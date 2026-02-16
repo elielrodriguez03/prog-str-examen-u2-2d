30e4b9ad515521b8fdca536d829b57e925a026b2
David-Jimenez-Gon2604 <20253rd001@utez.edu.mx>
2026-02-16 08:52:34 -0600

    Se crea la caja automatica

--- ExamenU2/src/Main.java
+++ ExamenU2/src/Main.java
@@ -2,19 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         double subtotal = 0.0;
 
         while (true) {
+
             System.out.print("Precio (0 para terminar): ");
 
             if (!sc.hasNextDouble()) {
                 System.out.println("Entrada inválida");
                 return;
             }
+
             double p = sc.nextDouble();
 
-             //Si p es 0 entonces break
+            // Si p es 0 entonces termina captura
+            if (p == 0) {
+                break;
+            }
 
             if (!U2Service.esPrecioValido(p)) {
                 System.out.println("Precio inválido");
@@ -30,9 +36,11 @@ public class Main {
             System.out.println("Entrada inválida");
             return;
         }
+
         int metodo = sc.nextInt();
 
-        if () { //Validacion de metodo de pago de 1 o 2
+        // Validación método 1 o 2
+        if (metodo != 1 && metodo != 2) {
             System.out.println("Método inválido");
             return;
         }
@@ -41,8 +49,8 @@ public class Main {
         double total = U2Service.calcularTotal(subtotal, descuento);
 
         System.out.println("=== TICKET ===");
-        System.out.println("Subtotal: "+subtotal);
-        System.out.println("Descuento: "+ descuento);
-        System.out.println("Total: "+ total);
+        System.out.printf("Subtotal: %.2f\n", subtotal);
+        System.out.printf("Descuento: %.2f\n", descuento);
+        System.out.printf("Total: %.2f\n", total);
     }
 }
--- ExamenU2/src/U2Service.java
+++ ExamenU2/src/U2Service.java
@@ -1,22 +1,31 @@
 public class U2Service {
 
+    // válido si p > 0
     public static boolean esPrecioValido(double p) {
-        // válido si p > 0
-        return false; // TODO
+        return p > 0;
     }
 
     public static double calcularSubtotal(double subtotalActual, double precio) {
-        return 0.0; // TODO
+        return subtotalActual + precio;
     }
 
     public static double calcularDescuento(double subtotal, int metodoPago) {
-        // >=500 y efectivo (1): 8%
-        // >=500 y tarjeta (2): 5%
-        // <500: 0
-        return 0.0; // TODO
+
+        if (subtotal >= 500) {
+
+            if (metodoPago == 1) { // efectivo
+                return subtotal * 0.08;
+            }
+
+            if (metodoPago == 2) { // tarjeta
+                return subtotal * 0.05;
+            }
+        }
+
+        return 0.0;
     }
 
     public static double calcularTotal(double subtotal, double descuento) {
-        return 0.0; // TODO
+        return subtotal - descuento;
     }
 }
